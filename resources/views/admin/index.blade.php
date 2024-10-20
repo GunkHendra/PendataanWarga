c3bce31e67d2cc3e978b14753365f0eb613c18da
Gung Hendra <gunghencrypt@gmail.com>
2024-10-20 15:20:50 +0800

    Mau pull

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,7 +19,7 @@
             <img src="https://via.placeholder.com/150" alt="Profile Picture" class="w-full h-full object-cover">
         </div>
         <div>
-            <h2 class="text-3xl font-bold text-white">Rp. 2.000.000</h2>
+            <h2 class="text-3xl font-bold text-white">+ Rp. 2.000.000</h2>
             <h2 class="text-xl font-bold text-black">Total Jumlah Pelunasan Iuran</h2>
         </div>
     </div>
@@ -35,22 +35,43 @@
         </div>
     </div>
 
-    {{-- Total Warga yang Sudah Lunas --}}
+    {{-- Total Penambahan Pendatang Bulan ini --}}
     <div class="bg-emerald-400 rounded-lg shadow-lg flex flex-col text-center items-center space-y-4 p-6">
         <div class="w-24 h-24 rounded-full overflow-hidden border-4 border-gray-300">
             <img src="https://via.placeholder.com/150" alt="Profile Picture" class="w-full h-full object-cover">
         </div>
         <div>
-            <h2 class="text-3xl font-bold text-white">30</h2>
-            <h2 class="text-xl font-bold text-black">Total Warga yang Sudah Lunas</h2>
+            <h2 class="text-3xl font-bold text-white">+3</h2>
+            <h2 class="text-2xl font-bold text-black">Total Pendatang Bulan Ini</h2>
         </div>
     </div>
 
-    {{-- Illustrasi --}}
-    {{-- <div class="col-span-3 bg-emerald-400 overflow-hidden rounded-lg shadow-lg border-2 border-black">
-        <div class="w-32 h-32 overflow-hidden">
-            <img src="/assets/dashboard-illustrasi/infinity-838683.webp" alt="Profile Picture">
+    {{-- Total Iuran Belum Lunas --}}
+    <div class="bg-emerald-400 rounded-lg shadow-lg flex flex-col text-center items-center space-y-4 p-6">
+        <div class="w-24 h-24 rounded-full overflow-hidden border-4 border-gray-300">
+            <img src="https://via.placeholder.com/150" alt="Profile Picture" class="w-full h-full object-cover">
         </div>
-    </div> --}}
+        <div>
+            <h2 class="text-3xl font-bold text-white">- Rp. 2.000.000</h2>
+            <h2 class="text-xl font-bold text-black">Total Iuran Belum Lunas</h2>
+        </div>
+    </div>
+
+    {{-- Total Warga Belum Lunas --}}
+    <div class="bg-emerald-400 rounded-lg shadow-lg flex flex-col text-center items-center space-y-4 p-6">
+        <div class="w-24 h-24 rounded-full overflow-hidden border-4 border-gray-300">
+            <img src="https://via.placeholder.com/150" alt="Profile Picture" class="w-full h-full object-cover">
+        </div>
+        <div>
+            <h2 class="text-3xl font-bold text-white">20</h2>
+            <h2 class="text-xl font-bold text-black">Total Warga Belum Lunas</h2>
+        </div>
+    </div>
 </div>
+{{-- Illustrasi --}}
+{{-- <div class="col-span-3 bg-emerald-400 overflow-hidden rounded-lg shadow-lg border-2 border-black">
+    <div class="w-32 h-32 overflow-hidden">
+        <img src="/assets/dashboard-illustrasi/infinity-838683.webp" alt="Profile Picture">
+    </div>
+</div> --}}
 @endsection
